ef3b77b38dfa8e217aee40ee6c59b5a06654d04b
Aryan <tony4737stark@gmail.com>
2026-07-16 11:40:39 +0530

    removed

--- backend/check_import.js
+++
@@ -1,9 +0,0 @@
-(async ()=>{
-  try {
-    await import('./src/app.js');
-    console.log('IMPORT_OK');
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-})();
--- backend/prisma/migrations/20260715182329_add_unique_custom_slug/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[customSlug]` on the table `Url` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Url_customSlug_key" ON "Url"("customSlug");
